3f8e246e6a7b0f098be163b98529111771932e48
Shamim <imsiddiquee@gmail.com>
2021-09-15 17:44:05 +0600

    update

--- force-app/main/default/aura/SecondAuraComponent/SecondAuraComponentController.js
+++ force-app/main/default/aura/SecondAuraComponent/SecondAuraComponentController.js
@@ -1,6 +1,40 @@
 ({
   handleClick: function (component, event, helper) {
-    var formData = component.get("v.examObj");
+    var formData = component.get("v.contactObj");
+    //console.log(JSON.stringify(formData));
     console.log(JSON.stringify(formData));
+
+    var action = component.get("c.saveContact");
+    action.setParams({ con: formData });
+    console.log("Save");
+
+    action.setCallback(this, function (response) {
+      var state = response.getState();
+      console.log("Contact Save" + state);
+      if (state === "SUCCESS") {
+        helper.showToast(
+          component,
+          event,
+          helper,
+          "Contact created successfully!"
+        );
+      } else if (state === "ERROR") {
+        var errorMessage = "Unknown error";
+
+        var errors = response.getError();
+        if (errors) {
+          if (errors[0] && errors[0].message) {
+            errorMessage = errors[0].message;
+          }
+        }
+
+        helper.showToast(component, event, helper, errorMessage);
+      }
+
+      var returnConObj = response.getReturnValue();
+      console.log("return Con Obj" + JSON.stringify(returnConObj));
+    });
+
+    $A.enqueueAction(action);
   }
 });
--- force-app/main/default/aura/SecondAuraComponent/SecondAuraComponentHelper.js
+++ force-app/main/default/aura/SecondAuraComponent/SecondAuraComponentHelper.js
@@ -1,3 +1,11 @@
 ({
-  helperMethod: function () {}
+  showToast: function (component, event, helper, message) {
+    var toastEvent = $A.get("e.force:showToast");
+    toastEvent.setParams({
+      type: "Success",
+      title: "Success!",
+      message: message
+    });
+    toastEvent.fire();
+  }
 });
